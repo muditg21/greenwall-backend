b66a113bceb5f659a7cf38b13c8f47d2f2aa04f6
muditg21 <muditgupta340@gmail.com>
2025-12-19 15:49:34 +0530

    added token bearer

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,20 +1,20 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-    try {
-        const token = req.header("Authorization");
-
-        if (!token) {
-            return res.status(401).json({ error: "access denied" });
-        }
-        const verified = jwt.verify(token, process.env.JWT_SECRETS);
-
-        req.user = verified;
-        next();
-    } catch (err) {
-        res.status(400).json({error:"invalid token"});
+  try {
+    const authHeader = req.header("Authorization");
+    if (!authHeader) {
+      return res.status(401).json({ error: "Access denied" });
     }
 
+    const token = authHeader.split(" ")[1]; // Bearer token
+    const verified = jwt.verify(token, process.env.JWT_SECRETS);
+
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).json({ error: "Invalid token" });
+  }
 };
 
-module.exports=verifyToken;
+module.exports = verifyToken;
